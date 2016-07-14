8dde81072fa902b3b3a30572184d637f4efa818b
hapjs <mailhap@qq.com>
2016-07-14 21:39:11 +0800

    init

---
+++ bin/cli.js
@@ -0,0 +1,18 @@
+#!/bin/sh
+':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
+
+var fs = require('fs');
+var path = require('path');
+var md5 = require('../index.js');
+
+var target = process.argv[2];
+var pathToTarget = target;
+
+if (fs.existsSync(pathToTarget)) {
+	fs.readFile(pathToTarget, function(err, buf) {
+		if(err) throw err;
+		console.log('file md5: ' + md5(buf));
+	});
+} else {
+	console.log('string md5: ' + md5(target));
+}
